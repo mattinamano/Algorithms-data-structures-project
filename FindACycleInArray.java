f4884119557901d1c556e4a43843722c518618fa
Mattina Mano Manoharan <47929228+mattinamano@users.noreply.github.com>
2019-09-26 17:34:34 -0700

    Create FindACycleInArray.java

---
+++ FindACycleInArray.java
@@ -0,0 +1,48 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+ class FindACycleInArray{
+     boolean isACycle(int[] arr)
+    {
+        int p=0,q=0;
+        while(true)
+        {
+            try{
+                if(q>arr.length||p>arr.length||p<0||q<0)
+                    return false;
+                q=arr[q];
+                if(q==p)
+                    return true;
+                if(q>arr.length||q<0)
+                    return false;
+                q=arr[q];
+                if(q==p)
+                    return true;
+                p=arr[p];
+                if(p>arr.length||p<0)
+                    return false;
+                if(q==p)
+                    return true;
+            }
+            catch(ArrayIndexOutOfBoundsException e)
+            {
+                return false;
+            }
+        }
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+	    FindACycleInArray f=new FindACycleInArray();
+		int[] arr = new int[]{1,2,4,5,7,6,3,8};
+		if(f.isACycle(arr)==true)
+		System.out.println(""+f.isACycle(arr));
+		else
+		System.out.println("No Cycle Found");
+	}
+}
