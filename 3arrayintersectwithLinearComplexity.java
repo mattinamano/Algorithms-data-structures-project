bc403fd764fea007bce50f6b8d6505e374b6546c
Mattina Mano Manoharan <47929228+mattinamano@users.noreply.github.com>
2019-09-30 20:06:42 -0700

    Create 3arrayintersectwithLinearComplexity.java

---
+++ 3arrayintersectwithLinearComplexity.java
@@ -0,0 +1,67 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+  
+import java.util.*;
+class IntersectionBetweenThreeArrays
+{
+    int[] theIntersectArray(int[] arr1,int[] arr2,int[] arr3)
+    {
+        int count=0;
+        int[] intersect=new int[]{};
+        HashMap<Integer,Boolean> hm1=new HashMap<Integer,Boolean>();
+        for(int i=0;i<arr1.length;i++)
+        {
+             hm1.put(arr1[i],true);
+        }
+        HashMap<Integer,Boolean> hm2=new HashMap<Integer,Boolean>();
+        for(int i=0;i<arr2.length;i++)
+        {
+             hm2.put(arr2[i],true);
+        }
+        HashMap<Integer,Boolean> hm3=new HashMap<Integer,Boolean>();
+        for(int i=0;i<arr3.length;i++)
+        {
+             hm3.put(arr3[i],true);
+        }
+        for(int j=0;j<arr1.length;j++)
+        {
+             if(hm2.containsKey(arr1[j])==true)
+             {  
+                 hm2.put(arr1[j],false);
+            }
+        }
+        for(int j=0;j<arr2.length;j++)
+        {
+             if(hm3.containsKey(arr2[j])==true&&hm3.get(arr2[j])==false)
+             {  
+                 hm3.put(arr2[j],false);
+            }
+        }
+
+        for(int j=0;j<arr3.length;j++)
+        {
+             if(hm3.get(arr3[j])==false)
+             {  
+                 intersect[count++]=arr3[j];
+            }
+        }
+        return intersect;
+    }
+}
+public class Main{
+	public static void main(String[] args) 
+	{
+	    int[] arr1 = new int[]{1,3,5,3,0};
+	    int[] arr2 = new int[]{0,5,3,3,13,10};
+	    int[] arr3 = new int[]{3,7,11,5,3,0,9};
+	    IntersectionBetweenThreeArrays i=new IntersectionBetweenThreeArrays();
+	    int[] theIntersect=i.theIntersectArray(arr1,arr2,arr3);
+	    for(int k=0;k<theIntersect.length;k++)
+	    System.out.println(theIntersect[k]+" ");
+	}
+}
